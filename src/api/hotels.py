e79830e988acaf5942889e898c483150ec95e685
Valery Shkarubin <valis660@mail.ru>
2025-05-26 15:12:24 +0300

    replace like with contains

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -20,14 +20,15 @@ async def get_hotels(
     async with async_session_maker() as session:
         query = select(HotelsOrm)
         if location:
-            query = query.filter(func.lower(HotelsOrm.location).like(f"%{location.strip().lower()}%"))
+            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
         if title:
-            query = query.filter(func.lower(HotelsOrm.title).like(f"%{title.strip().lower()}%"))
+            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
         query = (
             query
             .limit(per_page)
             .offset(per_page * (pagination.page - 1))
         )
+        # print(query.compile(compile_kwargs={"literal_binds": True}))
         result = await session.execute(query)
         hotels = result.scalars().all()
         return hotels
