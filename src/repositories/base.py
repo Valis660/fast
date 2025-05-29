d9bbf25045ffc5203cd363305edbd6b23a3a82b4
Valery Shkarubin <valis660@mail.ru>
2025-05-29 16:30:13 +0300

    create 'add' method for BaseRepository, use it for POST /hotels

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,4 +1,5 @@
-from sqlalchemy import select
+from sqlalchemy import select, insert
+from pydantic import BaseModel
 
 class BaseRepository:
     model = None
@@ -18,3 +19,7 @@ class BaseRepository:
         result = await self.session.execute(query)
         return result.scalars().one_or_none()
 
+    async def add(self, data: BaseModel):
+        add_data_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+        result = await self.session.execute(add_data_stmt)
+        return result.scalars().one()
