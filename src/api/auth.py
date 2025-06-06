5765a3765c7028821ecbaf79ff83c6456576efdd
Valery Shkarubin <valis660@mail.ru>
2025-06-06 19:13:58 +0300

    create AuthService

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,34 +1,24 @@
-from datetime import timedelta, datetime, timezone
 from fastapi import APIRouter, HTTPException
 
-from passlib.context import CryptContext
-
-import jwt
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
+from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+@router.post("/register")
+async def register_user(
+        data: UserRequestAdd,
+):
+    hashed_password = AuthService().hash_password(data.password)
+    new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+    async with async_session_maker() as session:
+        await UsersRepository(session).add(new_user_data)
+        await session.commit()
 
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode |= {"exp": expire}
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    return {"status": "OK"}
 
 
 @router.post("/login")
@@ -39,20 +29,8 @@ async def login_user(
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
             raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован!")
-        if not verify_password(data.password, user.hashed_password):
+        if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail="Пароль не верный!")
-        access_token = create_access_token({"user_id": user.id})
+        access_token = AuthService().create_access_token({"user_id": user.id})
         return {"access_token": access_token}
 
-
-@router.post("/register")
-async def register_user(
-        data: UserRequestAdd,
-):
-    hashed_password = pwd_context.hash(data.password)
-    new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
-    async with async_session_maker() as session:
-        await UsersRepository(session).add(new_user_data)
-        await session.commit()
-
-    return {"status": "OK"}
