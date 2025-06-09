66abd3f1e45097812a2df0675b3dfba29e9b61a4
Valery Shkarubin <valis660@mail.ru>
2025-06-09 14:39:24 +0300

    return current user in /only_auth route

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response, Request
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
@@ -23,7 +23,8 @@ async def register_user(
 
 @router.post("/login")
 async def login_user(
-        data: UserRequestAdd
+        data: UserRequestAdd,
+        response: Response,
 ):
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
@@ -32,5 +33,16 @@ async def login_user(
         if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail="Пароль не верный!")
         access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
 
+@router.get("/only_auth")
+async def only_auth(
+    request: Request,
+):
+    access_token = request.cookies.get("access_token", None)
+    data = AuthService().decode_token(access_token)
+    user_id = data['user_id']
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(id=user_id)
+    return user
