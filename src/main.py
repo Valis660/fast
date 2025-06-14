ed71aa2b5ead9932b150d7f78fabc04e18b128af
Valery Shkarubin <valis660@mail.ru>
2025-06-14 13:08:18 +0300

    create /hotels/{hotel_id}/rooms router; add get_filtered() function to BaseRepository

--- src/main.py
+++ src/main.py
@@ -7,10 +7,13 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
+from src.api.rooms import router as router_rooms
+from src.api.auth import router as router_auth
 app = FastAPI()
 
+app.include_router(router_auth)
 app.include_router(router_hotels)
-
+app.include_router(router_rooms)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
