8aca2674a9910a79be00b343381cdfc6b69c3163
Valery Shkarubin <valis660@mail.ru>
2025-06-17 18:05:19 +0300

    add POST /bookings

--- src/models/bookings.py
+++ src/models/bookings.py
@@ -9,11 +9,11 @@ from src.database import Base
 class BookingsOrm(Base):
     __tablename__ = "bookings"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
-    date_from: Mapped[date] = mapped_column(primary_key=True)
-    date_to: Mapped[date] = mapped_column(primary_key=True)
+    date_from: Mapped[date]
+    date_to: Mapped[date]
     price: Mapped[int]
 
     @hybrid_property
