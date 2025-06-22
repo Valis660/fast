2882882f60b081b259e3f4b505cf750495a8af98
Valery Shkarubin <valis660@mail.ru>
2025-06-22 13:05:15 +0300

    create get_filtered_by_time() in RoomsRepository

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -1,15 +1,18 @@
-from fastapi import APIRouter, Body
+from datetime import date
 
-from src.database import async_session_maker
-from src.repositories.rooms import RoomsRepository
+from fastapi import APIRouter, Body, Query
+
 from src.schemas.rooms import RoomAdd, RoomAddRequest, RoomPatch, RoomPatchRequest
-from src .api.dependencies import DBDep
+from src.api.dependencies import DBDep
 router = APIRouter(prefix="/hotels", tags=["Номера"])
 
 
 @router.get("/{hotel_id}/rooms/")
-async def get_rooms(db: DBDep, hotel_id: int):
-    return await db.rooms.get_filtered(hotel_id=hotel_id)
+async def get_rooms(db: DBDep,
+        hotel_id: int,
+        date_from: date = Query(example='2025-07-01'),
+        date_to: date = Query(example='2025-07-10'),):
+    return await db.rooms.get_filtered_by_time(hotel_id=hotel_id, date_from=date_from, date_to=date_to)
 
 
 @router.get("/{hotel_id}/rooms/{room_id}")
