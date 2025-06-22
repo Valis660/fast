1cd8738177dc11dbaa5bc3a4a72c0623d1e64caf
Valery Shkarubin <valis660@mail.ru>
2025-06-22 16:08:46 +0300

    change get_filtered(), return rooms instead of null

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -40,11 +40,23 @@ class RoomsRepository(BaseRepository):
             .cte(name="rooms_left_table")
         )
 
-        query = (
-            select(rooms_left_table)
-            .select_from(rooms_left_table)
-            .filter(rooms_left_table.c.rooms_left > 0)
+        rooms_ids_for_hotel = (
+            select(RoomsOrm.id)
+            .select_from(RoomsOrm)
+            .filter_by(hotel_id=hotel_id)
+            .subquery(name="rooms_ids_for_hotel")
         )
 
-        print(query.compile(bind=engine, compile_kwargs={"literal_binds": True}))
+        rooms_ids_to_get = (
+            select(rooms_left_table.c.room_id)
+            .select_from(rooms_left_table)
+            .filter(
+                rooms_left_table.c.rooms_left > 0,
+                        rooms_left_table.c.room_id.in_(rooms_ids_for_hotel),
+            )
+        )
+
+        print(rooms_ids_to_get.compile(bind=engine, compile_kwargs={"literal_binds": True}))
+
+        return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
 
