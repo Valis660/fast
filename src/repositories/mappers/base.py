81bfbf1e73378e70df4e9e1a87d222705b6e4493
Valery Shkarubin <valis660@mail.ru>
2025-06-30 12:38:00 +0300

    create DataMapper and HotelMapper

---
+++ src/repositories/mappers/base.py
@@ -0,0 +1,12 @@
+class DataMapper:
+    db_model = None
+    schema = None
+
+
+    @classmethod
+    def map_to_domains_entity(cls, data):
+        return cls.schema.model_validate(data, from_attributes=True)
+
+    @classmethod
+    def map_to_persistence_entity(cls, data):
+        return cls.db_model(**data.model_dump())
