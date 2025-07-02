34ec880bf4dffd73c44dcc8cd5f34ac8d319493f
Valery Shkarubin <valis660@mail.ru>
2025-07-02 12:24:57 +0300

    add lifespan, use RedisManager to cache /uslugi

--- src/api/uslugi.py
+++ src/api/uslugi.py
@@ -1,12 +1,26 @@
+import json
 from fastapi import APIRouter, Body
 
+from src.init import redis_manager
 from src.schemas.uslugi import UslugiAdd
 from src.api.dependencies import DBDep
 router = APIRouter(prefix="/uslugi", tags=["Удобства"])
 
 @router.get("")
 async def get_uslugi(db: DBDep):
-    return await db.uslugi.get_all()
+    uslugi_from_cache = await redis_manager.get("uslugi")
+    print(f"{uslugi_from_cache=}")
+    if not uslugi_from_cache:
+        print("ИДУ В БАЗУ ДАННЫХ")
+        uslugi = await db.uslugi.get_all()
+        uslugi_schemas: list[dict] = [f.model_dump() for f in uslugi]
+        uslugi_json = json.dumps(uslugi_schemas)
+        await redis_manager.set("uslugi", uslugi_json, 10)
+
+        return uslugi
+    else:
+        uslugi_dict = json.loads(uslugi_from_cache)
+        return uslugi_dict
 
 @router.post("")
 async def create_uslugi(db: DBDep, uslugi_data: UslugiAdd = Body()):
--- src/main.py
+++ src/main.py
@@ -1,18 +1,29 @@
-from fastapi import FastAPI
 import uvicorn
-
 import sys
+from fastapi import FastAPI
 from pathlib import Path
+from contextlib import asynccontextmanager
+
 
 sys.path.append(str(Path(__file__).parent.parent))
 
+from src.init import redis_manager
 from src.api.auth import router as router_auth
 from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
 from src.api.uslugi import router as router_uslugi
 from src.api.bookings import router as router_bookings
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # При старте приложения
+    await redis_manager.connect()
+    yield
+    # При выключении/перезагрузке приложения
+    await redis_manager.close()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router_auth)
 app.include_router(router_hotels)
