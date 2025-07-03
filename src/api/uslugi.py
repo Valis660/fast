3a6d28d24c64a8b5374624bfa120f5bfd6082620
Valery Shkarubin <valis660@mail.ru>
2025-07-03 17:51:49 +0300

    add celery and use simple task

--- src/api/uslugi.py
+++ src/api/uslugi.py
@@ -3,6 +3,8 @@ from fastapi_cache.decorator import cache
 
 from src.schemas.uslugi import UslugiAdd
 from src.api.dependencies import DBDep
+from src.tasks.tasks import test_task
+
 router = APIRouter(prefix="/uslugi", tags=["Удобства"])
 
 @router.get("")
@@ -15,4 +17,8 @@ async def get_uslugi(db: DBDep):
 async def create_uslugi(db: DBDep, uslugi_data: UslugiAdd = Body()):
     uslugi = await db.uslugi.add(uslugi_data)
     await db.commit()
+
+    #test_tasks.delay()
+
     return {"Status": "OK", "data": uslugi}
+
