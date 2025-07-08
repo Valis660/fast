56ad170a9be72a5a39c6cd6ade99f8060665a1fe
Valery Shkarubin <valis660@mail.ru>
2025-07-08 10:50:37 +0300

    refactor db and ac context managers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import json
+from typing import Any, AsyncGenerator
 
 import pytest
 from httpx import AsyncClient
+
 from src.config import settings
 from src.database import Base, engine_null_pool, async_session_maker_null_pool
 from src.main import app
@@ -16,33 +18,46 @@ def check_test_mode():
     assert settings.MODE == "TEST"
 
 
+@pytest.fixture(scope="function")
+async def db() -> AsyncGenerator[Any, Any]:
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        yield db
+
+
 @pytest.fixture(scope="session", autouse=True)
 async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
-        with open("tests/mock_hotels.json", encoding="utf-8") as file_hotels:
-            hotels = json.load(file_hotels)
-        with open("tests/mock_rooms.json", encoding="utf-8") as file_rooms:
-            rooms = json.load(file_rooms)
+    with open("tests/mock_hotels.json", encoding="utf-8") as file_hotels:
+        hotels = json.load(file_hotels)
+    with open("tests/mock_rooms.json", encoding="utf-8") as file_rooms:
+        rooms = json.load(file_rooms)
 
         hotels = [HotelAdd.model_validate(hotel) for hotel in hotels]
         rooms = [RoomAdd.model_validate(room) for room in rooms]
 
-        async with DBManager(session_factory=async_session_maker_null_pool) as db:
-            await db.hotels.add_bulk(hotels)
-            await db.rooms.add_bulk(rooms)
-            await db.commit()
+    async with DBManager(session_factory=async_session_maker_null_pool) as db_:
+        await db_.hotels.add_bulk(hotels)
+        await db_.rooms.add_bulk(rooms)
+        await db_.commit()
+
+
+@pytest.fixture(scope="session")
+async def ac() -> AsyncGenerator[Any, None]:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def register_user(setup_database):
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        await ac.post(
-            "/auth/register",
-            json={
-                "email": "kot@pes.ru",
-                "password": "12345"
-            }
-        )
+async def register_user(ac, setup_database):
+    await ac.post(
+        "/auth/register",
+        json={
+            "email": "kot@pes.ru",
+            "password": "12345"
+        }
+    )
+
+
