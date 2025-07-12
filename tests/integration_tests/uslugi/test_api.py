a274f4e57b432edc4a195a703c4fb9986bed0e31
Valery Shkarubin <valis660@mail.ru>
2025-07-12 11:08:59 +0300

    add tests for uslugi

--- tests/integration_tests/uslugi/test_api.py
+++ tests/integration_tests/uslugi/test_api.py
@@ -1,7 +1,14 @@
 async def test_get_uslugi(ac):
-    response = await ac.get(
-        "/uslugi"
-    )
-    print(f"{response.json()=}")
-
+    response = await ac.get("/uslugi")
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+async def test_post_uslugi(ac):
+    uslugi_title = "БАНЬКА-ПАРИЛКА"
+    response = await ac.post("/uslugi", json={"title": uslugi_title})
+    assert response.status_code == 200
+    res = response.json()
+    assert isinstance(res, dict)
+    assert res["data"]["title"] == uslugi_title
+    assert "data" in res
