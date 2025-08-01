fc1be6b356ef19744e6af1e25789b50ba2506d5d
Valery Shkarubin <valis660@mail.ru>
2025-08-01 19:42:34 +0300

    add api test test_add_and_get_my_bookings()

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,5 +1,8 @@
 import pytest
 
+from tests.conftest import get_db_null_pool
+
+
 @pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
     (1, "2025-08-01", "2025-08-10", 200),
     (1, "2025-08-02", "2025-08-11", 200),
@@ -28,3 +31,37 @@ async def test_add_booking(
         assert res["status"] == "OK"
         assert isinstance(res, dict)
         assert "data" in res
+
+@pytest.fixture(scope="module")
+async def delete_all_bookings():
+    async for _db in get_db_null_pool():
+        await _db.bookings.delete()
+        await _db.commit()
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, booked_rooms", [
+    (1, "2025-08-01", "2025-08-10", 1),
+    (1, "2025-08-02", "2025-08-11", 2),
+    (1, "2025-08-03", "2025-08-12", 3),
+])
+async def test_add_and_get_my_bookings(
+        room_id,
+        date_from,
+        date_to,
+        booked_rooms,
+        delete_all_bookings,
+        authenticated_ac,
+):
+    response = await authenticated_ac.post(
+        "/bookings",
+        json={
+            "room_id": room_id,
+            "date_from": date_from,
+            "date_to": date_to,
+        }
+    )
+    assert response.status_code == 200
+
+    response_my_bookings = await authenticated_ac.get("/bookings/me")
+    assert response_my_bookings.status_code == 200
+    assert len(response_my_bookings.json()) == booked_rooms
